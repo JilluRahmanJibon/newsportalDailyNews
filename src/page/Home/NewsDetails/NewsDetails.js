d811b6393996467e8944c73853be30f6fe7325c3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-09 01:09:00 +0600

    NewsDetails page add

--- src/page/Home/NewsDetails/NewsDetails.js
+++ src/page/Home/NewsDetails/NewsDetails.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const NewsDetails = () => {
+  const detailsData = useLoaderData();
+  console.log(detailsData);
   return (
     <div>
-      <h1>this is NewsDetails</h1>
+      <h1 className="text-[28px] font-semibold">{detailsData?.title}</h1>
+      <img src={detailsData?.picture} alt="" />
+      <h1 className="text-[18px]">{detailsData?.description}</h1>
     </div>
   );
 };
