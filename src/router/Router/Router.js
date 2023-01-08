af95e18038da702383ab79c05f6b113606bf100c
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2023-01-08 18:22:46 +0600

    login and register page added

--- src/router/Router/Router.js
+++ src/router/Router/Router.js
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../../page/Home/Home/Home";
 import Main from "../../layout/Main";
+import ErrorPage from "../../ErrorPage/ErrorPage";
+import Login from "../../page/Login/Login";
+import Register from "../../page/Register/Register";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage />,
     element: <Main></Main>,
     children: [
       { path: "/", element: <Home></Home> },
       { path: "/home", element: <Home></Home> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+
     ],
   },
 ]);
