f175a47d1662d2c794196d99f1e0cf118cde3d11
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-08 22:39:59 +0600

    category data load

--- src/Shared/SecondNavbar/SecondNavbar.js
+++ src/Shared/SecondNavbar/SecondNavbar.js
@@ -1,53 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const SecondNavbar = () => {
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/news")
+      .then((res) => res.json())
+      .then((data) => setCategory(data));
+  }, []);
+  // console.log(category);
+  // category name
+  const categoryNames = category.map((category) => category.category);
+
+  //unique category name
+  const uniqueCategory = [...new Set(categoryNames)];
+  console.log(uniqueCategory);
+
   return (
     <div>
       <ul className="lg:flex gap-5 px-1 text-xs cursor-pointer justify-center items-center font-semibold mb-2 hidden ">
-        <li>
-          <Link>World</Link>
-        </li>
-        <li>
-          <Link>Business</Link>
-        </li>
-        <li>
-          <Link>Politics</Link>
-        </li>
-        <li>
-          <Link>U.S</Link>
-        </li>
-        <li>
-          <Link>Sports</Link>
-        </li>
-        <li>
-          <Link>Health</Link>
-        </li>
-        <li>
-          <Link>N.Y</Link>
-        </li>
-        <li>
-          <Link>Opinion</Link>
-        </li>
-        <li>
-          <Link>Tech</Link>
-        </li>
-        <li>
-          <Link>Science</Link>
-        </li>
-        <li>
-          <Link>Games</Link>
-        </li>
-        <li>
-          <Link>Newsletters</Link>
-        </li>
-
-        <li>
-          <Link>Podcasts</Link>
-        </li>
-        <li>
-          <Link>Times Store</Link>
-        </li>
+        {uniqueCategory.map((category, uxi) => (
+          <li key={uxi}>
+            <Link>{category}</Link>
+          </li>
+        ))}
       </ul>
       <hr className="w-[80%] mx-auto border-1 border-black mb-1" />
       <hr className="w-[80%] mx-auto border-1 border-black" />
