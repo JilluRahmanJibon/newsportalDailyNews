ab11d25b6c475298828caf75399b1eb7309432d4
Amit Sarker <amitsarker255@gmail.com>
2023-01-08 17:12:22 +0600

    second navbar

--- src/page/Home/Home/Home.js
+++ src/page/Home/Home/Home.js
@@ -3,7 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>this is home....</h1>
+      <div>
+        <h1 className="text-5xl font-bold text-center">Daily News</h1>
+      </div>
+      <div className="mx-52">
+        <h1>this is homePage....</h1>
+      </div>
     </div>
   );
 };
