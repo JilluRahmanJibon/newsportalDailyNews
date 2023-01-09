0a1b13390da4f5201ebb78239301060a69d7e651
abdulquayumhp <badulquayumhp998@gmailc.com>
2023-01-09 08:17:48 +0600

    hot topic designe

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -1,24 +1,26 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
 import { FiMenu } from "react-icons/fi";
-import { FaUserCircle } from 'react-icons/fa';
 
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
 const Navbar = () => {
-  const { user, userLogOut } = useContext(AuthContext)
+  const { user, userLogOut } = useContext(AuthContext);
+  console.log(user);
   const [userRole, setUserRole] = useState({});
   const [checkRole, setCheckRole] = useState(false);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_ApiUrl}current/user?email=${user?.email}`, {
-      headers: {
-        authorization: `bearer ${localStorage.getItem('access-token')}`
-      },
-    }).then(res => {
-      setUserRole(res.data);
-    })
-
+    axios
+      .get(`${process.env.REACT_APP_ApiUrl}current/user?email=${user?.email}`, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("access-token")}`,
+        },
+      })
+      .then((res) => {
+        setUserRole(res.data);
+      });
   }, [user?.email]);
   return (
     <div className="lg:mx-28 md:20 mx-10 relative">
@@ -44,45 +46,69 @@ const Navbar = () => {
             <li>
               <a>CANADA</a>
             </li>
-            {userRole?.role === 'publisher' && <li>
-              <Link to='/news/addnews'>Add News</Link>
-            </li>}
+            {userRole?.role === "publisher" && (
+              <li>
+                <Link to="/news/addnews">Add News</Link>
+              </li>
+            )}
           </ul>
         </div>
         <div className="">
-          {user?.email ?
+          {user?.email ? (
             <div>
               {/* publisher check and profile  */}
-              {userRole?.role === 'publisher' && <div>
-
-                <div onClick={() => setCheckRole(!checkRole)} className="w-8 h-8 rounded-full cursor-pointer">
-                  {user?.photoURL ? <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" /> : <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" />}
-                </div>
-                {
-                  checkRole && <div className="absolute right-0 bg-blue-500 w-[150px] p-3 rounded-tr-none rounded-lg text-white">
-                    <h1>Profile </h1>
-                    <button onClick={() => userLogOut()} className="">Log out</button>
+              {userRole?.role === "publisher" && (
+                <div>
+                  <div
+                    onClick={() => setCheckRole(!checkRole)}
+                    className="w-8 h-8 rounded-full cursor-pointer"
+                  >
+                    {user?.photoURL ? (
+                      <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" />
+                    ) : (
+                      <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" />
+                    )}
                   </div>
-                }
-              </div>}
+                  {checkRole && (
+                    <div className="absolute right-0 bg-blue-500 w-[150px] p-3 rounded-tr-none rounded-lg text-white">
+                      <h1>Profile </h1>
+                      <button onClick={() => userLogOut()} className="">
+                        Log out
+                      </button>
+                    </div>
+                  )}
+                </div>
+              )}
 
               {/* admin check and profile  */}
-              {userRole?.role === 'admin' && <div>
-
-                <div onClick={() => setCheckRole(!checkRole)} className="w-8 h-8 rounded-full cursor-pointer">
-                  {user?.photoURL ? <img src={user?.photoURL} className='w-8 h-8 border-2 border-red-400 rounded-full' alt="" /> : <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" />}
+              {userRole?.role === "admin" && (
+                <div>
+                  <div
+                    onClick={() => setCheckRole(!checkRole)}
+                    className="w-8 h-8 rounded-full cursor-pointer"
+                  >
+                    {user?.photoURL ? (
+                      <img
+                        src={user?.photoURL}
+                        className="w-8 h-8 border-2 border-red-400 rounded-full"
+                        alt=""
+                      />
+                    ) : (
+                      <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" />
+                    )}
+                  </div>
+                  {checkRole && <div>admin</div>}
                 </div>
-                {
-                  checkRole && <div>admin</div>
-                }
-              </div>}
+              )}
             </div>
-            : <Link
+          ) : (
+            <Link
               to="/login"
               className="text-white font-bold py-1 px-2 bg-blue-500 hover:bg-blue-700 rounded text-xs"
             >
               LOGIN
-            </Link>}
+            </Link>
+          )}
         </div>
       </div>
     </div>
--- src/page/Home/RelativePost/RelativeCard.js
+++ src/page/Home/RelativePost/RelativeCard.js
@@ -2,19 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const RelativeCard = ({ newsData }) => {
- 
   return (
     <Link to={`/news/${newsData?._id}`}>
       <h1 className="font-bold">{newsData?.category}</h1>
       <div>
         <div className="w-full lg:py-2  flex  ">
           <div>
-            <Link className="text-3xl font-bold">{newsData?.title}</Link>
+            <Link className="text-3xl font-bold">
+              {newsData?.title?.slice(0, 25)}
+            </Link>
             <p className="py-2 pr-2">{newsData?.description?.slice(0, 100)}</p>
           </div>
           <div className="px-2">
             <img
-              className="w-[400px] object-cover h-[200px] "
+              className="w-[300px] object-cover h-[200px] "
               src={newsData?.picture}
               alt="/"
             />
