246c0a92d6b92dc9f180b624cf1fcfac0f7ef0ba
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2023-01-09 10:15:10 +0600

    pera number 6

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
 
 
 
-  
+
   return (
     <div className="lg:mx-28 md:20 mx-10 relative">
       <div className="flex justify-between items-center ">
@@ -57,6 +57,20 @@ const Navbar = () => {
         <div className="">
           {user?.uid ?
             <div>
+
+              {userRole?.email === user?.email && userRole?.role === 'reader' && <div>
+
+                <div onClick={() => setCheckRole(!checkRole)} className="w-8 h-8 rounded-full cursor-pointer">
+                  {user?.photoURL ? <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" /> : <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" />}
+                </div>
+                {
+                  checkRole && <div className="absolute right-0 bg-blue-500 w-[150px] p-3 rounded-tr-none rounded-lg text-white">
+                    <h1>Reader  </h1>
+                    <button onClick={() => userLogOut()} className="">Log out</button>
+                  </div>
+                }
+              </div>}
+
               {/* publisher check and profile  */}
               {userRole?.email === user?.email && userRole?.role === 'publisher' && <div>
 
@@ -72,13 +86,16 @@ const Navbar = () => {
               </div>}
 
               {/* admin check and profile  */}
-              {userRole?.role === 'admin' && <div>
+              {userRole?.email === user?.email && userRole?.role === 'admin' && <div>
 
                 <div onClick={() => setCheckRole(!checkRole)} className="w-8 h-8 rounded-full cursor-pointer">
-                  {user?.photoURL ? <img src={user?.photoURL} className='w-8 h-8 border-2 border-red-400 rounded-full' alt="" /> : <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" />}
+                  {user?.photoURL ? <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" /> : <FaUserCircle className="w-8 h-8 rounded-full border-red-400 border-2" />}
                 </div>
                 {
-                  checkRole && <div>admin</div>
+                  checkRole && <div className="absolute right-0 bg-blue-500 w-[150px] p-3 rounded-tr-none rounded-lg text-white">
+                    <h1>Admin Profile </h1>
+                    <button onClick={() => userLogOut()} className="">Log out</button>
+                  </div>
                 }
               </div>}
             </div>
